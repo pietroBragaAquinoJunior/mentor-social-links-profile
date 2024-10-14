6002afea04fbb6824cf48eaec98ec2855ae673fc
Pietro Aquino <pietro.aquino@tcema.tc.br>
2024-10-14 09:53:19 -0300

    finalizado

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- <AppHeader /> -->
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="css">
+#app{
+  font-family: 'Inter';
+  font-size: 14px;
+  color: hsl(0, 0%, 100%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-[hsl(0,0%,8%)] h-[calc(100vh)] p-6 flex items-center justify-center">
+    <div class="bg-[hsl(0,0%,12%)] rounded-xl flex-1 md:p-10 flex flex-col p-6 mb-6 items-center md:max-w-[375px]">
+      <img class="rounded-full w-[88px] mb-6" src="/images/avatar-jessica.jpeg" alt="Avatar Jessica">
+      <h1 class="font-[700] text-[24px] mb-1">
+        Jessica Randall
+      </h1>
+      <p class="text-[hsl(75,94%,57%)] font-[600] mb-6">
+        London, United Kingdom
+      </p>
+      <p class="mb-6">
+        "Front-end developer and avid reader."
+      </p>
+      <div class="flex flex-col gap-4 w-full">
+        <button class="bg-[hsl(0,0%,20%)] h-[45px] rounded-lg font-[700]">
+          GitHub
+        </button>
+        <button class="bg-[hsl(0,0%,20%)] h-[45px] rounded-lg font-[700]">
+          Frontend Mentor
+        </button>
+        <button class="bg-[hsl(0,0%,20%)] h-[45px] rounded-lg font-[700]">
+          LinkedIn
+        </button>
+        <button class="bg-[hsl(0,0%,20%)] h-[45px] rounded-lg font-[700]">
+          Twitter
+        </button>
+        <button class="bg-[hsl(0,0%,20%)] h-[45px] rounded-lg font-[700]">
+          Instagram
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
